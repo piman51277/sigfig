e1b13b72d11f86c5178df9eb74f47c94759a83a8
piman <44853490+piman51277@users.noreply.github.com>
2021-08-20 18:59:57 -0400

    update eslint config

--- src/Scinumber.ts
+++ src/Scinumber.ts
@@ -10,6 +10,7 @@ export default class Scinumber {
 
 	//convert to number
 	toNumber(): number {
+
 		//FIXME fix JS float error! (1.1 == 1.1000000000000001)
 		return parseFloat(this.value) * Math.pow(10, this.power);
 	}
@@ -21,6 +22,7 @@ export default class Scinumber {
 
 	//normalize value
 	normalize(): void {
+
 		//parse this.value
 		const number = parseFloat(this.value);
 
@@ -34,10 +36,12 @@ export default class Scinumber {
 
 	//gets the max precision
 	getPrecision(): number {
+
 		//if there is no ., presision is this.power
 		if (this.value.indexOf('.') === -1) {
 			return this.power;
 		} else {
+
 			//return the power - the lowest decimal place, and add 2 to make up for decimal
 			return this.power - this.value.length + Math.max(0, this.value.indexOf('.') + 1);
 		}
@@ -45,6 +49,7 @@ export default class Scinumber {
 
 	//get Scinumber from number
 	static fromNumber(number: number): Scinumber {
+
 		// if number is 0
 		if (number === 0) {
 			return Scinumber.Zero();
--- src/SigFig.ts
+++ src/SigFig.ts
@@ -20,6 +20,7 @@ export default class SigFig extends Scinumber {
 
 	//rounds to 10^place and returns a Scinumber
 	roundTo(place: number): SigFig {
+
 		//FIXME for now, the outputs of this function have the same precision as the input. This may not be the case.
 		//TODO find the corrrect behavior of precision for rounding
 
@@ -30,12 +31,15 @@ export default class SigFig extends Scinumber {
 		if (truePlace * -1 > this.value.length - (Math.max(0, this.value.indexOf('.')) + 1)) {
 			return new SigFig(this.value, this.power, this.precision);
 		}
+
 		//if the value cannot affect the rounding at all, return 0
 		else if (truePlace > this.power + 1) {
 			return new SigFig('0', 0, this.precision);
 		}
+
 		//if the value can affect the rounding
 		else if (truePlace === this.power) {
+
 			//look at the first figure in this.value
 			const target = parseInt(this.value.charAt(0));
 
@@ -46,8 +50,10 @@ export default class SigFig extends Scinumber {
 				return new SigFig('1', this.power + 1, this.precision);
 			}
 		}
+
 		//every other case
 		else {
+
 			//get the true place in terms of nvalue
 			const workingPlace = truePlace * -1;
 
@@ -61,19 +67,23 @@ export default class SigFig extends Scinumber {
 			const targetNext = parseInt(nvalue.charAt(workingPlace + 1));
 
 			let newValueAtTarget = 0;
+
 			//if the next figure is less than 5, round down
 			if (targetNext < 5) {
 				newValueAtTarget = target;
 			}
+
 			//if the next figure is greater than 5, round up
 			else if (targetNext > 5) {
 				newValueAtTarget = target + 1;
 			}
+
 			//the next figure is a 5, round up if even
 			else {
 				newValueAtTarget = target + (target % 2 === 0 ? 0 : 1);
 			}
 
+			//replace the figure at true place with the new value
 			const newValue = nvalue.charAt(0) + '.' + nvalue.slice(1, workingPlace) + (newValueAtTarget).toString();
 			return new SigFig(newValue, this.power, this.precision);
 		}
