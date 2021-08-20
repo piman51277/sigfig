11bff8e91f0e4ff41894ebde53d2a787e7e88bc0
piman <44853490+piman51277@users.noreply.github.com>
2021-08-20 18:13:12 -0400

    add edge cases to Scinumber.fromNumber

--- src/Scinumber.ts
+++ src/Scinumber.ts
@@ -34,11 +34,25 @@ export default class Scinumber {
 
 	//get Scinumber from number
 	static fromNumber(number: number): Scinumber {
+		// if number is 0
+		if (number === 0) {
+			return Scinumber.Zero();
+		}
+
+		//if number is negative
+		let isNegative = false;
+		if (number < 0) {
+			isNegative = true;
+
+			//get absolute value
+			number = Math.abs(number);
+		}
+
 		//get log base 10 of number
 		const power = Math.floor(Math.log10(number));
 
 		//divide number by 10^power
-		const value = number / Math.pow(10, power);
+		const value = number / Math.pow(10, power) * (isNegative ? -1 : 1);
 
 		//return new Scinumber
 		return new Scinumber(value.toString(), power);
